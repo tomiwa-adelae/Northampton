d71861ddb41630e7c8fbec73c87f8ee39ce16a57
Tomiwa Adelae <thetommedia@gmail.com>
2025-03-12 13:32:33 +0100

    Updated the whatsapp number

--- constants/index.ts
+++ constants/index.ts
@@ -5,7 +5,7 @@ export const phoneNumbers = [
 	"08050399782",
 	"08157425111",
 ];
-export const whatsappNumber = "2348050399780";
+export const whatsappNumber = "2348157425111";
 export const address =
 	"51B, Agboola Ajumobi Crescent, Magodo Phase 2, Lagos State";
 
